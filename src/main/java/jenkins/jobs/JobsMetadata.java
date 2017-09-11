5ae90c71a678cfd4db4ce1e6e363c73010fa7e5e
Anshuman Ambasht <anshumanambasht@QAIT.COM>
2017-09-11 11:47:06 +0530

    removed url

--- src/main/java/jenkins/jobs/JobsMetadata.java
+++ src/main/java/jenkins/jobs/JobsMetadata.java
@@ -1,13 +1,7 @@
 package jenkins.jobs;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -15,19 +9,11 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.TimeZone;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
-import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import net.sf.json.JSONException;
-
 import com.offbytwo.jenkins.JenkinsServer;
 import com.offbytwo.jenkins.model.Job;
 
@@ -88,35 +74,6 @@ public class JobsMetadata extends JenkinsAuth {
 		return queue;
 	}
 
-	public static JSONObject readJsonFromUrl(String url) throws IOException,
-			JSONException {
-		InputStream is = new URL(url).openStream();
-		try {
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is,
-					Charset.forName("UTF-8")));
-			String jsonText = readAll(rd);
-			JSONObject json = null;
-			try {
-				json = new JSONObject(jsonText);
-			} catch (org.json.JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return json;
-		} finally {
-			is.close();
-		}
-	}
-
-	private static String readAll(Reader rd) throws IOException {
-		StringBuilder sb = new StringBuilder();
-		int cp;
-		while ((cp = rd.read()) != -1) {
-			sb.append((char) cp);
-		}
-		return sb.toString();
-	}
-
 	public Queue<String> getDesiredJobs() {
 		Queue<String> queue = new LinkedList<String>();
 		queue.add("SMOKE_SBRC_PROD_Suite");
@@ -125,7 +82,7 @@ public class JobsMetadata extends JenkinsAuth {
 	
 	public static void extractTime() throws MalformedURLException, IOException{
 		try{
-		Document doc = Jsoup.connect("http://go.qa.ggtest.com/ps/systemHealth").get();
+		Document doc = Jsoup.connect("").get();
 		Elements element = doc.select("#buildDate");
 			System.out.println(StringUtils.substringAfter(element.text(), " ").replace("EDT", ""));
 		}
